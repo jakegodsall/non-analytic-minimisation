841a97e74a150917925d21e97135dce71b1213ff
jakegodsall <jake.edward.godsall@gmail.com>
2022-07-11 09:52:27 +0100

    Removed log likelihood function and fixed MultivariateGaussian.pdf()

--- src/data_modelling.py
+++ src/data_modelling.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.stats as stats
 
 
 class MultivariateGaussian:
@@ -8,6 +7,7 @@ class MultivariateGaussian:
         a nxd array where n = the number of samples and d is the number of
         random variables or parameters.
     """
+
     def __init__(self):
         self.mu_ = None
         self.covmat_ = None
@@ -27,35 +27,11 @@ class MultivariateGaussian:
 
           Returns: the probability
         """
+        x = x[:, np.newaxis]  # add a new first dimension to x
         k = self.mu_.shape[0]  # number of dimensions
         diff = x - self.mu_  # deviation of x from the mean
+        inv_covmat = np.linalg.inv(self.covmat_)
 
-        term1 = 1. / (np.sqrt(2 * np.pi) ** k * np.linalg.det(self.covmat_))
-        term2 = np.exp(-(np.linalg.solve(self.covmat_, diff.T).dot(diff)) / 2)
+        term1 = (2 * np.pi) ** -(k / 2) * np.linalg.det(inv_covmat)
+        term2 = np.exp(-np.einsum('ijk, kl, ijl->ij', diff, inv_covmat, diff) / 2)
         return term1 * term2
-
-    def probability_from_mesh(self, xx, yy):
-        """
-            Generate an array of probability values from a given meshgrid
-            of xx and yy
-            Note: xx and yy must be the same shape.
-        """
-        pdf_values = np.zeros((xx.shape[0], yy.shape[0]))
-
-        for i in range(xx.shape[0]):
-            for j in range(xx.shape[1]):
-                pdf_values[i][j] = mg.pdf([xx[i][j], yy[i][j]])
-
-        return pdf_values
-
-    def log_likelihood(self, x):
-        k = self.mu_.shape[0]  # number of dimensions
-        n = x.shape[1]  # number of samples
-        inv_cov = np.linalg.inv(self.covmat_)  # inverse of the covariance matrix
-        diff = x - self.mu_  # deviation of x from the mean
-        maha_dist = np.sum(np.tensordot(diff.T, np.dot(inv_cov, diff), 1))
-
-        term1 = n*k*np.log(2*np.pi)
-        term2 = n*np.log(np.linalg.det(inv_cov))
-        term3 = maha_dist
-        return 0.5 * (term1 + term2 + term3)
