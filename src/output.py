d633450fe3f1c5bb011b68e799647a1bac17bd68
Jake Godsall <jake.edward.godsall@gmail.com>
2022-07-18 12:22:51 +0100

    added output.py with OutputGenerator class for generating a json of results and metadata

---
+++ src/output.py
@@ -0,0 +1,28 @@
+import inspect
+import datetime
+
+
+class OutputGenerator:
+    def __init__(self, model, callback):
+        self.date = datetime.datetime.today()
+        self.objective_function = self.get_objective(model)
+        self.number_of_iterations = callback.shape[0]
+        self.results = callback.drop("iteration", axis=1).to_json(orient="split")
+
+    def get_objective(self, model):
+        if type(model) == "function":
+            lines = inspect.getsourcelines(model)
+            function = lines[0][-1].replace("return", "").strip()
+            return function
+        else:
+            return model
+
+    def return_json(self):
+        return {
+            "meta_data": {
+                "date": self.date,
+                "objective_function": self.objective_function,
+                "number_of_iterations": self.number_of_iterations,
+            },
+            "results": self.results
+        }
