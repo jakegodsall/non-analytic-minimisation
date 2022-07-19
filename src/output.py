e167dbb73d5996a22a0fad85b403167c7d5cc5f9
Jake Godsall <jake.edward.godsall@gmail.com>
2022-07-19 09:19:22 +0100

    Added method to the output JSON

--- src/output.py
+++ src/output.py
@@ -6,6 +6,7 @@ class OutputGenerator:
     def __init__(self, model, callback):
         self.date = datetime.datetime.today().strftime("%H:%M:%S %d/%m/%Y")
         self.objective_function = self.get_objective(model)
+        self.method = "Nelder-Mead"
         self.number_of_iterations = callback.shape[0]
         self.results = callback.drop("iteration", axis=1).to_json()
 
@@ -19,6 +20,7 @@ class OutputGenerator:
             "meta_data": {
                 "date": self.date,
                 "objective_function": self.objective_function,
+                "method": self.method,
                 "number_of_iterations": self.number_of_iterations,
             },
             "results": self.results
