17933b182218d98ca0c5cb9ef2ec362d739976ce
Jake Godsall <jake.edward.godsall@gmail.com>
2022-07-21 17:17:02 +0100

    Expanded TestObject into two distinct classes for unvariate Gaussian UnivariateTestObject and multivariate Gaussian MultivariateTestObject

--- src/data_modelling.py
+++ src/data_modelling.py
@@ -4,7 +4,7 @@ from scipy.optimize import minimize
 from src.callback import Simulator
 
 
-class TestObject:
+class UnivariateTestObject:
     def __init__(self):
         # random number generation hyperparameters
         self.random_state = None
@@ -32,29 +32,19 @@ class TestObject:
         rng = np.random.default_rng(random_state)
 
         # generate a random sample using the numpy function
-        sample = rng.normal(mu, variance, sample_size)
+        return rng.normal(mu, variance, size=sample_size)
 
-        return sample
+    def pdf(self, x):
+        """
+            Return a vector (shape of x) of the probabilities
+            of the values of x
+        """
+        return 1 / (np.sqrt(2 * np.pi * self.variance)) * np.exp(-1 / (2 * self.variance) * (x - self.mu) ** 2)
 
-    # define the uni-variate Gaussian likelihood function
-    def univariate_likelihood(self, params, x):
+    def likelihood(self, params, x):
         mu = self.model(params, x)
         likelihood = sum((x - mu) ** 2)
         return likelihood
-
-    def multivariate_likelihood(self, params, x):
-        mu = params
-        covmat = self.variance
-
-        x = x[:, np.newaxis]
-        k = mu.shape[0]
-        n = x.shape[1]
-        inv_covmat = np.linalg.inv(covmat)
-        diff = x - mu
-        maha_dist = np.einsum('ijk, kl, ijl->ij', diff, inv_covmat, diff)
-        return maha_dist
-
-
 
     def minimise(self, x, initial_guess, method, tolerance=1e-6):
         self.initial_guess = initial_guess
@@ -75,24 +65,39 @@ class TestObject:
         return lik_model
 
 
-class MultivariateGaussian:
-    """
-        Class for fitting a multivariate gaussian distribution to
-        a nxd array where n = the number of samples and d is the number of
-        random variables or parameters.
-    """
-
+class MultivariateTestObject:
     def __init__(self):
-        self.mu_ = None
-        self.covmat_ = None
+        # random number generation hyperparameters
+        self.random_state = None
+        self.mu = None
+        self.variance = None
+        self.sample_size = None
+        # optimisation hyperparameters
+        self.model = None
+        self.initial_guess = None
+        self.method = None
+        self.tolerance = None
 
-    def fit(self, x):
-        """
-            Fits the mean and covariance matrix for given input data x
-            to attributes mu_ and covmat_ respectively.
-        """
-        self.mu_ = x.mean(0)
-        self.covmat_ = np.dot((x - self.mu_).T, (x - self.mu_)) / x.shape[0]
+    @classmethod
+    def change_xdims(self, x):
+        return x[:, np.newaxis]
+
+    def generate_random_sample(self,
+                               random_state,
+                               mu,
+                               variance,
+                               sample_size):
+        # set object variables
+        self.random_state = random_state
+        self.mu = mu
+        self.variance = variance
+        self.sample_size = sample_size
+
+        # set the random seed
+        rng = np.random.default_rng(random_state)
+
+        # generate a random sample using the numpy function
+        return rng.multivariate_normal(mu, variance, size=sample_size)
 
     def pdf(self, x):
         """
@@ -101,11 +106,41 @@ class MultivariateGaussian:
 
           Returns: the probability
         """
-        x = x[:, np.newaxis]  # add a new first dimension to x
-        k = self.mu_.shape[0]  # number of dimensions
-        diff = x - self.mu_  # deviation of x from the mean
-        inv_covmat = np.linalg.inv(self.covmat_)
+        x = self.change_xdims(x)  # add a new first dimension to x
+        k = self.mu.shape[0]  # number of dimensions
+        diff = x - self.mu  # deviation of x from the mean
+        inv_covmat = np.linalg.inv(self.variance)
 
         term1 = (2 * np.pi) ** -(k / 2) * np.linalg.det(inv_covmat)
         term2 = np.exp(-np.einsum('ijk, kl, ijl->ij', diff, inv_covmat, diff) / 2)
         return term1 * term2
+
+    def likelihood(self, params, x):
+        mu = params
+        covmat = self.variance
+
+        x = self.change_xdims(x)
+        k = mu.shape[0]
+        n = x.shape[1]
+        inv_covmat = np.linalg.inv(covmat)
+        diff = x - mu
+        maha_dist = np.einsum('ijk, kl, ijl->ij', diff, inv_covmat, diff)
+        return maha_dist
+
+    def minimise(self, x, initial_guess, method, tolerance=1e-6):
+        self.initial_guess = initial_guess
+        self.method = method
+        self.tolerance = tolerance
+
+        # instantiate the simulator class
+        lik_sim = Simulator(self.likelihood)
+
+        # minimise the -log(L) function using the wrapper class Simulator
+        lik_model = minimize(lik_sim.simulate,
+                             x0=initial_guess,
+                             args=(x),
+                             method=method,
+                             tol=tolerance,
+                             options={"disp": True})
+
+        return lik_model
